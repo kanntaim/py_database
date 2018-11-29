f3f6f7d8c4a1b6ac31311b4fb11de9d07897f0c0
kanntaim <galileo174rus@gmail.com>
2018-11-29 18:36:30 +0300

    deleted "self"

--- union_reporting.py
+++ union_reporting.py
@@ -6,7 +6,7 @@ from tables.items.project_item import ProjectItem
 
 class UnionReporting:
     @staticmethod
-    def general_sql(self, ItemClass, query, params=None, **kwargs):
+    def general_sql(ItemClass, query, params=None, **kwargs):
         db = Database(database="union_reporting", user='n.galeev')
         table = db.execute_select_query(query, params)
         tests = []
@@ -19,12 +19,12 @@ class UnionReporting:
         return tests
 
     @staticmethod
-    def get_tests_min_launch_time(self):
+    def get_tests_min_launch_time():
         query = "select name, start_time " \
-            "from test "
-        self.general_sql(TestItem, query, test_name=None, execution_time=None)
+                "from test "
+        UnionReporting.general_sql(TestItem, query, test_name=None, execution_time=None)
 
     @staticmethod
-    def get_tests_since_time(self, time):
+    def get_tests_since_time( time):
         query = ""
-        self.general_sql(TestItem, params=time, project_name=None, test_name=None, start_time=None)
+        UnionReporting.general_sql(TestItem, params=time, project_name=None, test_name=None, start_time=None)
