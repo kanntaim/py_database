28db604476f13661277f573cc28c8c3e90f34f45
kanntaim <galileo174rus@gmail.com>
2018-11-30 12:04:27 +0300

    final test

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from union_reporting import UnionReporting
+import datetime
 
-rows = UnionReporting.get_tests_since_time('\'7.11.2015\'')
+date = datetime.date(2015, 11, 7)
+rows = UnionReporting.get_tests_min_launch_time()
+for row in rows:
+    print(row, end='\n')
+print('\n')
+
+rows = UnionReporting.get_project_tests_count()
+for row in rows:
+    print(row, end='\n')
+print('\n')
+
+rows = UnionReporting.get_tests_since_time(date)
+for row in rows:
+    print(row, end='\n')
+print('\n')
+
+rows = UnionReporting.get_browser_test_launches_count()
 for row in rows:
     print(row, end='\n')
--- union_reporting.py
+++ union_reporting.py
@@ -1,11 +1,13 @@
 from database import Database
+from items.browser_item import BrowserItem
+from items.project_item import ProjectItem
 from items.test_item import TestItem
 
 
 class UnionReporting:
     @staticmethod
     def general_sql(ItemClass, query, params=None, **kwargs):
-        db = Database(database="union_reporting", user='n.galeev')
+        db = Database(database='union_reporting', user='n.galeev')
         table = db.execute_select_query(query, params)
         tests = []
         for row in table:
@@ -19,13 +21,30 @@ class UnionReporting:
 
     @staticmethod
     def get_tests_min_launch_time():
-        query = "select name, start_time " \
-                "from test "
-        return UnionReporting.general_sql(TestItem, query, test_name=None, execution_time=None)
+        query = 'select p.name, t.name, min(timestampdiff(second, t.start, t.end)) ' \
+                'from test t join project p on t.project_id = p.id ' \
+                'group by p.name, t.name ' \
+                'order by by p.name, t.name'
+        return UnionReporting.general_sql(TestItem, query, project_name=None, test_name=None, execution_time=None)
 
     @staticmethod
     def get_tests_since_time(time):
-        query = "select name, start_time " \
-                "from test " \
-                "where start_time>=%s"
-        return UnionReporting.general_sql(TestItem, params=time, test_name=None, start_time=None)
+        query = 'select p.name, t.name, t.start_time ' \
+                'from test t join project p on t.project_id = p.id ' \
+                'where t.start_time>=%s ' \
+                'order by by p.name, t.name'
+        return UnionReporting.general_sql(TestItem, query, params=time, test_name=None, start_time=None)
+
+    @staticmethod
+    def get_project_tests_count():
+        query = 'select p.name, count(distinct t.name) ' \
+                'from test t join project p on t.project_id = p.id ' \
+                'group by p.name'
+        return UnionReporting.general_sql(ProjectItem, query, project=None, distinct_tests=None)
+
+    @staticmethod
+    def get_browser_test_launches_count():
+        query = 'select \'Chrome\', (select count(id) from test where browser = chrome ' \
+                'union ' \
+                'select \'Firefox\', (select count(id) from test where browser = firefox'
+        return UnionReporting.general_sql(BrowserItem, query, browser=None, tests_launched=None)
